9f6ead81efeac12d3123cd25a1ab060bb79c3e82
Sudalai02 <dev.sudalai@gmail.com>
2025-10-24 03:45:56 -0700

    secnd commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCn4mlQFKUUlEZYsGHsMcZMdjpkW31OhuM",
+  authDomain: "admin-pro-b81e3.firebaseapp.com",
+  projectId: "admin-pro-b81e3",
+  storageBucket: "admin-pro-b81e3.firebasestorage.app",
+  messagingSenderId: "58443942643",
+  appId: "1:58443942643:web:0f0c5ac8b1e919632e4586"
 };
 
 const app = initializeApp(firebaseConfig);
