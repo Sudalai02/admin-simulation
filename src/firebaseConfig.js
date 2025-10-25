4f85269a8d68da707deeee566676fce76db56c74
Sudalai02 <dev.sudalai@gmail.com>
2025-10-24 23:00:01 -0700

    secnd commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCn4mlQFKUUlEZYsGHsMcZMdjpkW31OhuM",
-  authDomain: "admin-pro-b81e3.firebaseapp.com",
-  projectId: "admin-pro-b81e3",
-  storageBucket: "admin-pro-b81e3.firebasestorage.app",
-  messagingSenderId: "58443942643",
-  appId: "1:58443942643:web:0f0c5ac8b1e919632e4586"
+  apiKey: "AIzaSyBS6w_Ru-iKsa_NryyZTnJXg7HuQb5nZgY",
+  authDomain: "admin-simulations.firebaseapp.com",
+  projectId: "admin-simulations",
+  storageBucket: "admin-simulations.firebasestorage.app",
+  messagingSenderId: "449586268337",
+  appId: "1:449586268337:web:867d79a7aef7461485ba90"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db =getFirestore(app)
 export const provider = new GoogleAuthProvider();
